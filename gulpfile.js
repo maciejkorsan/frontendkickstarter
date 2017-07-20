e675241db67793f2ccf91d2e3f7c76b42de5456c
Maciej Korsan <maciejkorsan@gmail.com>
2017-07-20 17:47:31 +0200

    feat: ES 2015 & eslint support

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-/* gulpfile by Maciej Korsan 04.2017 */
+/* gulpfile by Maciej Korsan 07.2017 */
 
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
@@ -9,14 +9,24 @@ var headerfooter = require('gulp-headerfooter');
 var browserSync = require('browser-sync').create();
 var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
+var eslint = require('gulp-eslint');
 
 gulp.task('assets', function() {
-  return gulp.src('./app/assets/**/*') 
+  return gulp.src('./app/assets/**/*')
         .pipe(gulp.dest('./dist/assets/'));
 });
 
-gulp.task('scripts', function() { 
+gulp.task('scripts', function() {
     gulp.src('app/js/app.js')
+        .pipe(eslint())
+        .on('error', function(err){
+            browserSync.notify(err.message, 3000);
+            this.emit('end');
+        })
+        .pipe(babel({
+            presets: ['es2015']
+        }))
         .pipe(browserify())
         .pipe(uglify())
         .pipe(gulp.dest('./dist/js'))
@@ -33,7 +43,7 @@ gulp.task('sass', function () {
 gulp.task('html', function() {
   return gulp.src('./app/content/*.html')
         .pipe(headerfooter.header('./app/partials/header.html'))
-        .pipe(headerfooter.footer('./app/partials/footer.html'))   
+        .pipe(headerfooter.footer('./app/partials/footer.html'))
         .pipe(gulp.dest('./dist/'));
 });
 
@@ -41,7 +51,7 @@ gulp.task('serve', ['sass','html','scripts','assets'], function() {
     browserSync.init({
         server: "./dist"
     });
-    gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("app/scss/**/*", ['sass']);
     gulp.watch("app/content/*.html", ['html']);
     gulp.watch("app/partials/*.html", ['html']);
     gulp.watch("app/js/*.js", ['scripts']);
